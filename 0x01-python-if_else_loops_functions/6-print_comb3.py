17a3d413aabedf2b145c5bd0a4547824bffb47eb
T33C33 <teeceeiheukwumere@gmail.com>
2024-03-12 09:51:56 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# prints all possible different combinations of two digits
+
+for first in range(10):
+    for second in range(first + 1, 10):
+        if first == 8 and second == 9:
+            print("{}{}".format(first, second))
+        else:
+            print("{}{}".format(first, second), end=", ")
