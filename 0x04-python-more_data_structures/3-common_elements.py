6dbdff72b544df664c9eb4b452c4a0315516dd22
T33C33 <teeceeiheukwumere@gmail.com>
2024-04-12 06:36:41 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     result = {}
@@ -6,3 +7,10 @@ def common_elements(set_1, set_2):
             if first == second:
                 result = first
     return result
+'''
+# This only returns single individual characters
+
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    Result = set_1 & set_2
+    return Result
