e2580295c9149dc363b1cbb60fb56aad0e4f0595
T33C33 <teeceeiheukwumere@gmail.com>
2024-04-12 07:33:00 -0700

    5-main.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for items in a_dictionary:
-        if items != None:
-            count = len(a_dictionary)
-        else:
-            count = 0
+    count = len(a_dictionary)
     return count
