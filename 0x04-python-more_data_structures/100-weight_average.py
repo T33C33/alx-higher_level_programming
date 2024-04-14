d8d69b30e0db4017ef625590151c9b9d7c3d7749
T33C33 <teeceeiheukwumere@gmail.com>
2024-04-14 16:32:24 -0700

    100-main.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    s, w = 0, 0
+
+    for score, weight in my_list:
+        s += score * weight
+        w += weight
+
+    return (s / w)
