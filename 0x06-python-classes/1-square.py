7b21c874c7fb44c35745820b79f84c6c0ea07d1c
T33C33 <teeceeiheukwumere@gmail.com>
2024-04-27 07:02:14 -0700

    1-main.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""A class that defines a square"""
+
+
+class Square:
+    """A class that defines a square"""
+
+    def __init__(self, size):
+        """
+         size(int): size of the square.
+        """
+        self.__size = size
