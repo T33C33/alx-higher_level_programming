8010054c6ac20860406e801199f655b0467462c8
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-20 17:16:14 +0100

    extra

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-let text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < text.length; i++){
 	console.log(text[i]);}
