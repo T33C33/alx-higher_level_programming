80725eb0860bf65b34638003b68bca9425bd1f33
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-20 18:00:12 +0100

    extra

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,14 @@ const args = process.argv;
 const x = parseInt(args[2], 10);
 
 if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else if (x <= 0) {
-    return;
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
+  console.log("Missing number of occurrences");
+  return;
+}
+
+if (x <= 0) {
+  return;
+}
+
+for (let i = 0; i < x; i++) {
+  console.log("C is fun");
 }
