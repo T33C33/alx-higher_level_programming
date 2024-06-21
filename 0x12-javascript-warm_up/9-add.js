c0d6cefca38f3c40ec76ba1d1e4d93dfdc42ee3b
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-21 04:11:39 +0100

    extra

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-function add (a, b) {
-  const first = parseFloat(a);
-  const second = parseFloat(b);
-  const result = first + second;
-
-  if ((first === undefined || isNaN(first)) && (second === undefined || isNaN(second))) {
-    console.log('NaN');
-  } else {
-    console.log(result);
-  }
+function add(a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
 add(Number(process.argv[2]), Number(process.argv[3]));
