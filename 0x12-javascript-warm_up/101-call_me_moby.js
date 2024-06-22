7d534b316078fabdca5989707eca9bd51be1ee73
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-22 13:03:33 +0100

    extra

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, extFunc) {
-    for (let i = 0; i < x; i++) {
-        extFunc();
-    }
-}
+  for (let i = 0; i < x; i++) {
+    extFunc();
+  }
+};
