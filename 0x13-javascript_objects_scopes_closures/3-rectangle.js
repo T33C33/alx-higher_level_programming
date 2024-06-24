7fdc51909ad508a1a94477bc0c5e535f63b11ccf
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-24 13:05:46 +0100

    extra

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let matrix = '';
+      for (let j = 0; j < this.width; j++) {
+        matrix += 'x';
+      }
+      console.log(matrix);
+    }
+  }
+}
+module.exports = Rectangle;
