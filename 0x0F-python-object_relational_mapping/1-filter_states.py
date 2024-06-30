bd2b2ffd2a36701c26a495467dafff8e5021396e
T33C33 <teeceeiheukwumere@gmail.com>
2024-06-30 15:45:30 +0100

    check

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,10 +3,7 @@
 import MySQLdb
 import sys
 
-"""
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
