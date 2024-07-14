26600f2d079ebf59762de1369ece4b5b98e9837e
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-15 00:07:07 +0100

    new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
 
     # Send a GET request as urllib.request to the specified URL
     with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
+        body = response.read()
 
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
