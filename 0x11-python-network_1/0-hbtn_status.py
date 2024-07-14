b636aa8a9cf05f4b84c5d3f9f0d32764fbe04ad2
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-14 23:55:05 +0100

    new

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Fetches https://alx-intranet.hbtn.io/status using urllib"""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    # Send a GET request as urllib.request to the specified URL
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
