bec77514b9a12c1e6e89d33fd20eaee4bc852d3b
T33C33 <teeceeiheukwumere@gmail.com>
2024-07-14 19:59:48 +0100

    new

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
 
-"""
-This module provides a function to find a peak element in a list of integers using a recursive binary search algorithm.
-"""
-
 def find_peak(list_of_integers):
     """
-    Finds a peak element in a list of integers using a recursive binary search algorithm.
+    Find a peak element in a list of integers.
 
     Args:
-        list_of_integers (list): The list of integers to search for a peak.
+        list_of_integers (list): A list of integers.
 
     Returns:
-        int: The peak element in the list, or None if the list is empty.
-    """
+        int: The peak element in the list.
 
+    Examples:
+        >>> find_peak([1, 2, 3, 4, 5])
+        5
+        >>> find_peak([5, 4, 3, 2, 1])
+        5
+        >>> find_peak([1, 3, 2, 4, 5])
+        3
+    """
     if not list_of_integers:
         return None
-
-    def find_peak_util(nums, low, high):
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
         mid = (low + high) // 2
-        
-        """is mid peak?"""
-        if (mid == 0 or nums[mid] >= nums[mid - 1]) and (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
-            return nums[mid]
-        
-        """if mid is not peak and element on left is greater than mid, then peak is on left"""
-        if mid > 0 and nums[mid - 1] > nums[mid]:
-            return find_peak_util(nums, low, mid - 1)
-        
-        """if mid is not peak and element on right is greater than mid, then peak is on right"""
-        return find_peak_util(nums, mid + 1, high)
-
-    return find_peak_util(list_of_integers, 0, len(list_of_integers) - 1)
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
