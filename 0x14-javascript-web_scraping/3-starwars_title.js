f3c956ef4fc23e62d5635e97b623b2b7186243e7
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-01 01:21:49 +0100

    new

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// prints an item from the title of a Star Wars movie
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
