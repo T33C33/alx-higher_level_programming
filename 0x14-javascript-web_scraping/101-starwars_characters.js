11de1f5163c95b331c27175aed74b3b2026a9123
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-01 01:37:34 +0100

    new

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 
-const filmId = process.argv[2];
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(filmUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
+    const content = JSON.parse(body);
+    const characters = content.characters;
     // console.log(characters);
     for (const characterUrl of characters) {
       request.get(characterUrl, (error, response, body) => {
