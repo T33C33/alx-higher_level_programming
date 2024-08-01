74f3a2af5d3cb44b0e0a3537a458b3387d674d57
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-01 01:14:31 +0100

    new

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // reads and prints the content of a file
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf-8", function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
     return;
