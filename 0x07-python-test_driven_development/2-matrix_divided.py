fa40597c09003663302166e2d8a872b7f50cb1e2
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-13 13:14:06 +0100

    new

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,32 +3,42 @@
 A function that divides all elements of a matrix.
 """
 
-
 def matrix_divided(matrix, div):
     """
     A function that divides all elements of a matrix.
 
     Args:
-        matrix (_type_): represents a matrix
-        div (_type_): represents a number to divide by
+        matrix (list of lists of int/float): The matrix to be divided.
+        div (int/float): The divisor.
+
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+        TypeError: If div is not a number.
+        ZeroDivisionError: If div is zero.
+
+    Returns:
+        list: A new matrix with each element divided by div.
+
+    Examples:
+        >>> matrix_divided([[3, 9], [12, 15]], -3)
+        [[-1.0, -3.0], [-4.0, -5.0]]
     """
+
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix \
-            (list of lists) of integers/floats")
-    if len(matrix[0]) != len(matrix[1]):
-        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_length = len(matrix[0])
     for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
         for element in row:
-            new_lst = []
             if type(element) not in [int, float]:
-                raise TypeError("matrix must be a matrix \
-                    (list of lists) of integers/floats")
-            if div < 0:
-                new_lst.append((round(element / (div * (-1)), 2)) * (-1))
-            else:
-                new_lst.append(round(element / div, 2))
-                return new_lst
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Create a new matrix with divided elements
+    new_lst = [[round(element / div, 2) for element in row] for row in matrix]
+    return new_lst
